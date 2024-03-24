e21c5127eb2a2baf9316029bd4f2eda5048028f3
chkrishnamurthy <chkrishnamurthy@gmail.com>
2024-03-24 12:00:00 +0530

    basic content added.

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -2,13 +2,35 @@ const Content = () => {
   return (
     <div className="font-source-code-pro font-mono text-white mt-16">
       <div className="flex flex-col gap-6">
-        <p className="text-4xl font-extrabold">{`<html>`}</p>
-        <p className="text-4xl font-extrabold">{`<head>`}</p>
-        <p className="text-4xl font-extrabold ml-4">{`<title>`}</p>
-        <span className="text-4xl text-gray-400 font-bold ml-8">
-          Personal Portfolio
+        <p className="text-3xl">{`<html>`}</p>
+        <p className="text-3xl">{`<head>`}</p>
+        <p className="text-3xl ml-4">{`<title>`}</p>
+        <span className="text-3xl text-gray-400 ml-8">Personal Portfolio</span>
+        <p className="text-3xl ml-4">{`</title>`}</p>
+        <p className="text-3xl">{`</head>`}</p>
+        <p className="text-3xl mb-4">{`<body>`}</p>
+
+        <p className="text-3xl ml-4">{`<div id="About">`}</p>
+        <span className="text-3xl text-gray-400 ml-8">About comes here</span>
+        <p className="text-3xl ml-4 mb-4">{`</div>`}</p>
+
+        <p className="text-3xl ml-4">{`<div id="Skills">`}</p>
+        <span className="text-3xl text-gray-400 ml-8">Skills comes here</span>
+        <p className="text-3xl ml-4 mb-4">{`</div>`}</p>
+
+        <p className="text-3xl ml-4">{`<div id="Work-Experience">`}</p>
+        <span className="text-3xl text-gray-400 ml-8">
+          Work Experience comes here
         </span>
-        <p className="text-4xl font-extrabold ml-4">{`</title>`}</p>
+        <p className="text-3xl ml-4 mb-4">{`</div>`}</p>
+
+        <p className="text-3xl ml-4">{`<div id="Projects">`}</p>
+        <span className="text-3xl text-gray-400 ml-8">Projects comes here</span>
+        <p className="text-3xl ml-4 mb-4">{`</div>`}</p>
+
+        <p className="text-3xl mt-4">{`</body>`}</p>
+
+        <p className="text-3xl">{`</html>`}</p>
       </div>
     </div>
   );
--- src/Components/Introduction.js
+++ src/Components/Introduction.js
@@ -3,7 +3,7 @@ const Introduction = () => {
     <>
       <span className="text-4xl sm:text-6xl text-white">Hello, I'm</span>
       <div className="flex items-center my-6 sm:mt-6">
-        <h2 className="text-4xl sm:text-6xl text-gray-500 text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-600">
+        <h2 className="text-4xl sm:text-6xl text-[#ff9d00] bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-600">
           Krishna Murthy
         </h2>
         <span className="text-4xl sm:text-6xl animate-pulse ease-in-out duration-300">
